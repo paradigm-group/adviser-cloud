26c4197528d346093835b575430ed0190133acb7
Jack Machin <jack.machin125@gmail.com>
2016-04-15 11:46:55 +0100

    Top

--- header.php
+++ header.php
@@ -45,7 +45,10 @@
                     <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
                     <a href="<?php echo home_url(); ?>" rel="nofollow"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" title="<?php bloginfo('name'); ?>" alt="<?php bloginfo('name'); ?>"></a>
                 </div>
+
                 <?php get_sidebar ('header');?>
+
+                <div class="top"></div>
             </div>
 
             <nav role="navigation">
