5e8e5a348b19d3f58b97f8bf23d8af91f59f4f58
Jack Machin <jack.machin125@gmail.com>
2016-04-15 09:41:02 +0100

    Issues

--- header.php
+++ header.php
@@ -43,10 +43,7 @@
             <div id="inner-header" class="wrapper">
                 <div class="logo-container">
                     <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-                    <p id="logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
-                    <?php // if you'd like to use the site description you can un-comment it below ?>
-                    <?php // bloginfo('description'); ?>
+                    <a href="<?php echo home_url(); ?>" rel="nofollow"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" title="<?php bloginfo('name'); ?>" alt="<?php bloginfo('name'); ?>"></a>
                 </div>
                 <?php get_sidebar ('header');?>
             </div>
