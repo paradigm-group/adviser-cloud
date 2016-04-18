549242ce30f3595e6c49055d06ca69c2de5b7a3e
Jack Machin <jack.machin125@gmail.com>
2016-04-18 10:39:19 +0100

    NAsty

--- single.php
+++ single.php
@@ -4,7 +4,7 @@
 
 				<div id="inner-content" class="container">
 
-					<div class="single" role="main">
+					<div class="eight" role="main">
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
